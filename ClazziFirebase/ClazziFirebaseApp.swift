1cd60180fab1a962ba7eaa2615bdcc763aebddfa
ksoheee <thgml0716030@naver.com>
2025-09-01 11:06:46 +0900

    Initial Commit

---
+++ ClazziFirebase/ClazziFirebaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClazziFirebaseApp.swift
+//  ClazziFirebase
+//
+//  Created by Admin on 9/1/25.
+//
+
+import SwiftUI
+
+@main
+struct ClazziFirebaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
